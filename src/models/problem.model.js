13699f494591ed19fae3d9baa7dda9528d711b3e
Manoj Kumar <manoj8894421801@gmail.com>
2024-05-05 21:18:07 +0530

    FEAT: prepared schema for problems

---
+++ src/models/problem.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const problemSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Title of the problem cannot be empty.']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description of the problem cannot be empty.']
+    },
+    difficulty: {
+        type: String,
+        enum: ['easy', 'medium', 'hard'],
+        default: 'easy',
+        required: [true, 'Difficulty of the problem cannot be empty.']
+    },
+    testcases: [
+        {
+            input: {
+                type: String,
+                required: [true, 'test case input cannot be empty.']
+            },
+            output: {
+                type: String,
+                required: [true, 'test case input cannot be empty.']
+            }
+        }
+    ],
+    editorial: {
+        type: String
+    }
+});
+
+
+const Problem = model('Problem', problemSchema);
+
+module.exports = Problem;
+
+/**
+ * [{input:'5',output:'10'},{input:'6',output:'12'}]
+ */
